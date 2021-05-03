8ad0df3a2b26cc6add8bd84e957ed481248545ba
Douglas Shibata <douglas.shibata11@gmail.com>
2021-05-03 12:33:47 -0300

    Static charts

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import ImgDsDark from 'assets/img/ImgDsDark.svg';
+import ImgDsDark from 'assets/img/ds-dark.svg';
 
 const NavBar = () => (
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
